c00239e5eab4e27c3824ca8cabf03d24e1a140cd
ChasVanDav <blackkoreangirl@gmail.com>
2024-09-10 21:01:45 -0400

    updated student card to hold event information

--- client/src/components/Student.jsx
+++ client/src/components/Student.jsx
@@ -1,28 +1,43 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import * as ioicons from 'react-icons/io5'
+import * as ioicons from 'react-icons/io5';
 
-const Student = ({student, toUpdate, toDelete}) => {
+const EventCard = ({ event, toUpdate, toDelete }) => {
 
-    const onUpdate = (toUpdateStudent) => {
-        toUpdate(toUpdateStudent)
+    const onUpdate = (toUpdateEvent) => {
+        toUpdate(toUpdateEvent);
     }
 
-    const onDelete = (toDeleteStudent) => {
-        toDelete(toDeleteStudent)
+    const onDelete = (toDeleteEvent) => {
+        toDelete(toDeleteEvent);
     }
 
     return (
         <Card>
             <Card.Body>
-            <Card.Title>{student.firstname} {student.lastname}</Card.Title>
-            <Button variant="outline-danger" onClick={()=>{onDelete(student)}} style={{padding: '0.6em', marginRight:'0.9em'}}><ioicons.IoTrash/></Button>
-            <Button variant="outline-info" onClick={()=>{onUpdate(student)}} style={{padding: '0.6em'}}> <ioicons.IoSync/></Button>
+                <Card.Title>{event.eventname}</Card.Title>
+                <Card.Text>
+                    Date: {event.date}
+                </Card.Text>
+                <Card.Text>
+                    Location: {event.location}
+                </Card.Text>
+                <Button 
+                    variant="outline-danger" 
+                    onClick={() => onDelete(event)} 
+                    style={{ padding: '0.6em', marginRight: '0.9em' }}>
+                    <ioicons.IoTrash />
+                </Button>
+                <Button 
+                    variant="outline-info" 
+                    onClick={() => onUpdate(event)} 
+                    style={{ padding: '0.6em' }}>
+                    <ioicons.IoSync />
+                </Button>
             </Card.Body>
         </Card>
-    )
-
+    );
 }
 
-export default Student;
+export default EventCard;
